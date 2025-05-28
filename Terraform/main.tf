a9e0c547db9809d71d5849cb34aaa96594ddc7ff
sushant14320 <51773383+sushant14320@users.noreply.github.com>
2025-05-28 13:29:41 +0000

    added code files

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -27,37 +27,37 @@ resource "azurerm_resource_group" "testrg" {
 resource "azurerm_virtual_network" "testvnet" {
   name                = var.vnetname
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.testrg.location
+  resource_group_name = azurerm_resource_group.testrg.name
 }
 
 resource "azurerm_subnet" "testsubnet" {
   name                 = "testsubnet"
-  resource_group_name  = azurerm_resource_group.main.name
+  resource_group_name  = azurerm_resource_group.testrg.name
   virtual_network_name = azurerm_virtual_network.main_vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_network_interface" "testnic" {
   name                = "nic"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.testrg.location
+  resource_group_name = azurerm_resource_group.testrg.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.main_subnet.id
+    subnet_id                     = azurerm_subnet.testsubnet.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "testvm" {
   name                = var.vmname
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.testrg.name
+  location            = azurerm_resource_group.testrg.location
   size                = "Standard_B1s"
   admin_username      = "admin"
   network_interface_ids = [
-    azurerm_network_interface.main_nic.id,
+    azurerm_network_interface.testnic.id,
   ]
 
 
@@ -77,8 +77,8 @@ resource "azurerm_linux_virtual_machine" "testvm" {
 
 resource "azurerm_storage_account" "main_storage" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.main.name
-  location                 = azurerm_resource_group.main.location
+  resource_group_name      = azurerm_resource_group.testrg.name
+  location                 = azurerm_resource_group.testrg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }

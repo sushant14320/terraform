34537d8237e6f1c9fa863a454350ecd539f4d4a2
sushant14320 <51773383+sushant14320@users.noreply.github.com>
2025-05-28 13:30:43 +0000

    added code files

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -24,6 +24,7 @@ resource "azurerm_resource_group" "testrg" {
   location = "East US"
 }
 
+
 resource "azurerm_virtual_network" "testvnet" {
   name                = var.vnetname
   address_space       = ["10.0.0.0/16"]
@@ -34,7 +35,7 @@ resource "azurerm_virtual_network" "testvnet" {
 resource "azurerm_subnet" "testsubnet" {
   name                 = "testsubnet"
   resource_group_name  = azurerm_resource_group.testrg.name
-  virtual_network_name = azurerm_virtual_network.main_vnet.name
+  virtual_network_name = azurerm_virtual_network.testvnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
